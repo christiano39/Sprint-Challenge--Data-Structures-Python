38a5fe1c62b4b17575eb458980c49e79d2e93d8d
christiano39 <carneson39@gmail.com>
2020-09-11 09:18:02 -0600

    refactor names algorithm

---
+++ names/binary_search_tree.py
@@ -0,0 +1,37 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif self.value > target:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        elif self.value <= target:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    
+
